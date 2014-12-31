5f13eba497abd2ac9695173649b24dcd44ca2f0e
MatthewGerber <gerber.matthew@gmail.com>
2014-12-31 14:50:46 -0500

    Added license header to all files.

--- Sensus.WinPhone/MainPage.xaml.cs
+++ Sensus.WinPhone/MainPage.xaml.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿#region copyright
+// Copyright 2014 The Rector & Visitors of the University of Virginia
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+ 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -20,7 +36,7 @@ namespace Sensus.WinPhone
             InitializeComponent();
 
             Forms.Init();
-            Content = new Sensus.UI.MainPage().ConvertPageToUIElement(this);
+            Content = new SensusUI.MainPage(null).ConvertPageToUIElement(this);
         }
 
         // TODO:  App.Get().OnStop()
