4690bcd0f46f8a12bd2d84dcad67744bae82fb9f
MatthewGerber <gerber.matthew@gmail.com>
2015-01-19 23:26:10 -0500

    License header refresh.

--- Sensus.Android/Probes/Context/AndroidSoundProbe.cs
+++ Sensus.Android/Probes/Context/AndroidSoundProbe.cs
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 #endregion
-
+ 
 using Android.Media;
 using SensusService;
 using SensusService.Probes.Context;
