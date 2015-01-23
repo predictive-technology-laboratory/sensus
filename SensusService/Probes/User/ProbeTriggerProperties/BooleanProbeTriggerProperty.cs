0f55568d80fb0f816ed15587cf292eb77983ff16
MatthewGerber <gerber.matthew@gmail.com>
2015-01-23 10:58:19 -0500

    Removed extra space in license headers.

--- SensusService/Probes/User/ProbeTriggerProperties/BooleanProbeTriggerProperty.cs
+++ SensusService/Probes/User/ProbeTriggerProperties/BooleanProbeTriggerProperty.cs
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 #endregion
- 
+
 namespace SensusService.Probes.User.ProbeTriggerProperties
 {
     public class BooleanProbeTriggerProperty : ProbeTriggerProperty
--- SensusUI/ProbePage.cs
+++ SensusUI/ProbePage.cs
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 #endregion
- 
+
 using SensusService.Probes;
 using SensusService.Probes.User;
 using SensusUI.UiProperties;
