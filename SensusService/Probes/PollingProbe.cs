d62cd49298728852f88278ed069ba20a978170e7
MatthewGerber <gerber.matthew@gmail.com>
2015-02-16 12:46:26 -0500

    Removed region/endregion directives, since they're messing up Xamarin Studio namespace refactoring.

--- SensusService/Probes/PollingProbe.cs
+++ SensusService/Probes/PollingProbe.cs
@@ -1,4 +1,3 @@
-﻿#region copyright
 // Copyright 2014 The Rector & Visitors of the University of Virginia
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,15 +11,12 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-using Newtonsoft.Json;
-
-
-#endregion
 
 using SensusUI.UiProperties;
 using System;
 using System.Collections.Generic;
 using System.Threading;
+using Newtonsoft.Json;
 
 namespace SensusService.Probes
 {
