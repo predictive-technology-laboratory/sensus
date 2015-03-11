63a6e70acdc23f1ee646eb70f70a9d51fcb90229
Matthew Gerber <gerber.matthew@gmail.com>
2015-03-11 00:34:49 -0400

    Preparing for Android Sensus release v0.7.0-prerelease.

--- Sensus.Android/Release.sh
+++ Sensus.Android/Release.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-# show usage
-if [ $1 -eq "--help" || $# -ne 5 ]; then
-    echo "Usage:  ./Release [version name] [keystore path] [keystore password] [prerelease] [github access token]"
-    exit 0
+if [ $# -ne 4 ]; then
+    echo "Usage:  ./Release.sh [version name] [keystore path] [keystore password] [github prerelease]"
+    echo "\t[version name]:  Version name, following semantic versioning guidelines (e.g., 0.3.1-prerelease)."
+    echo "\t[keystore path]:  Path to keystore file."
+    echo "\t[keystore password]:  Password used to open keystore and signing key (assumed to be the same)."
+    echo "\t[github prerelease]:  Whether or not the GitHub release should be marked as a prerelease (true/false)."
+    exit 1
 fi
 
 # grab latest develop commit
@@ -17,23 +20,35 @@ sed -E -i '' "s/android:versionName=\"[^\"]+\"/android:versionName=\"$1\"/g" Pro
 new_version_code=$((`grep -oE 'android:versionCode="[^"]+"' Properties/AndroidManifest.xml | grep -oE "[0-9]+"`+1))
 sed -E -i '' "s/android:versionCode=\"[^\"]+\"/android:versionCode=\"$new_version_code\"/g" Properties/AndroidManifest.xml
 
-# build and sign APK, quitting if there is an error
-xbuild /p:Configuration=Release /p:AndroidKeyStore=true /p:AndroidSigningKeyAlias=sensus /p:AndroidSigningKeyPass=$3 /p:AndroidSigningKeyStore=$2 /p:AndroidSigningStorePass=$3 Sensus.Android.csproj
+# show updates
+git difftool
+
+# build project
+xbuild /p:Configuration=Release Sensus.Android.csproj
 if [ $? -ne 0 ]; then 
-    echo "Error building and/or signing release."
+    echo "Error building release."
     exit $?;
 fi
 
-# merge develop into master
+# sign apk
+xbuild /t:SignAndroidPackage /p:Configuration=Release /p:AndroidKeyStore=true /p:AndroidSigningKeyAlias=sensus /p:AndroidSigningKeyPass=$3 /p:AndroidSigningKeyStore=$2 /p:AndroidSigningStorePass=$3 Sensus.Android.csproj
+if [ $? -ne 0 ]; then
+    echo "Error signing APK."
+    exit $?;
+fi
+
+# merge develop into master and push master to github
 git commit -a -m "Preparing for Android Sensus release v$1."
+git push
 git checkout master
 git merge develop
+git push
 
 # create tag for release
 tag_name="Android-v$1"
-git tag -a $tag_name
+git tag -a $tag_name -m "Tag for Android Sensus release v$1."
 git push origin $tag_name
 
 # draft github release
-curl --data "{\"tag_name\": \"$tag_name\",\"target_commitish\": \"master\",\"name\": \"Android Sensus release v$1\",\"body\": \"Release of Sensus for Android, version $1.\",\"draft\": false,\"prerelease\": $4}" https://api.github.com/repos/MatthewGerber/sensus/releases?access_token=:$5
+curl -u MatthewGerber --data "{\"tag_name\": \"$tag_name\",\"target_commitish\": \"master\",\"name\": \"Android Sensus release v$1\",\"body\": \"Release of Sensus for Android, version $1.\",\"draft\": false,\"prerelease\": $4}" https://api.github.com/repos/MatthewGerber/sensus/releases
 
