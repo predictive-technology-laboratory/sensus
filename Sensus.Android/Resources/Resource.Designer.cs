507437a6f47c5e68632bc7df25ff8df746dc4191
Matthew Gerber <gerber.matthew@gmail.com>
2015-05-20 00:24:19 -0400

    Updated packages.

--- Sensus.Android/Resources/Resource.Designer.cs
+++ Sensus.Android/Resources/Resource.Designer.cs
@@ -446,11 +446,11 @@ namespace Sensus.Android
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f050003
-			public const int ApplicationName = 2131034115;
+			// aapt resource value: 0x7f050014
+			public const int ApplicationName = 2131034132;
 			
-			// aapt resource value: 0x7f050002
-			public const int Hello = 2131034114;
+			// aapt resource value: 0x7f050013
+			public const int Hello = 2131034131;
 			
 			// aapt resource value: 0x7f050000
 			public const int app_id = 2131034112;
@@ -458,56 +458,56 @@ namespace Sensus.Android
 			// aapt resource value: 0x7f050001
 			public const int app_name = 2131034113;
 			
+			// aapt resource value: 0x7f050002
+			public const int com_facebook_image_download_unknown_error = 2131034114;
+			
+			// aapt resource value: 0x7f050003
+			public const int com_facebook_internet_permission_error_message = 2131034115;
+			
 			// aapt resource value: 0x7f050004
-			public const int com_facebook_image_download_unknown_error = 2131034116;
+			public const int com_facebook_internet_permission_error_title = 2131034116;
 			
 			// aapt resource value: 0x7f050005
-			public const int com_facebook_internet_permission_error_message = 2131034117;
+			public const int com_facebook_like_button_liked = 2131034117;
 			
 			// aapt resource value: 0x7f050006
-			public const int com_facebook_internet_permission_error_title = 2131034118;
+			public const int com_facebook_like_button_not_liked = 2131034118;
 			
 			// aapt resource value: 0x7f050007
-			public const int com_facebook_like_button_liked = 2131034119;
+			public const int com_facebook_loading = 2131034119;
 			
 			// aapt resource value: 0x7f050008
-			public const int com_facebook_like_button_not_liked = 2131034120;
+			public const int com_facebook_loginview_cancel_action = 2131034120;
 			
 			// aapt resource value: 0x7f050009
-			public const int com_facebook_loading = 2131034121;
+			public const int com_facebook_loginview_log_in_button = 2131034121;
 			
 			// aapt resource value: 0x7f05000a
-			public const int com_facebook_loginview_cancel_action = 2131034122;
+			public const int com_facebook_loginview_log_in_button_long = 2131034122;
 			
 			// aapt resource value: 0x7f05000b
-			public const int com_facebook_loginview_log_in_button = 2131034123;
+			public const int com_facebook_loginview_log_out_action = 2131034123;
 			
 			// aapt resource value: 0x7f05000c
-			public const int com_facebook_loginview_log_in_button_long = 2131034124;
+			public const int com_facebook_loginview_log_out_button = 2131034124;
 			
 			// aapt resource value: 0x7f05000d
-			public const int com_facebook_loginview_log_out_action = 2131034125;
+			public const int com_facebook_loginview_logged_in_as = 2131034125;
 			
 			// aapt resource value: 0x7f05000e
-			public const int com_facebook_loginview_log_out_button = 2131034126;
+			public const int com_facebook_loginview_logged_in_using_facebook = 2131034126;
 			
 			// aapt resource value: 0x7f05000f
-			public const int com_facebook_loginview_logged_in_as = 2131034127;
+			public const int com_facebook_send_button_text = 2131034127;
 			
 			// aapt resource value: 0x7f050010
-			public const int com_facebook_loginview_logged_in_using_facebook = 2131034128;
+			public const int com_facebook_share_button_text = 2131034128;
 			
 			// aapt resource value: 0x7f050011
-			public const int com_facebook_send_button_text = 2131034129;
+			public const int com_facebook_tooltip_default = 2131034129;
 			
 			// aapt resource value: 0x7f050012
-			public const int com_facebook_share_button_text = 2131034130;
-			
-			// aapt resource value: 0x7f050013
-			public const int com_facebook_tooltip_default = 2131034131;
-			
-			// aapt resource value: 0x7f050014
-			public const int messenger_send_button_text = 2131034132;
+			public const int messenger_send_button_text = 2131034130;
 			
 			static String()
 			{
