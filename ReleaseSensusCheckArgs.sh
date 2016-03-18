48bf46e08aa257de0fad8544da4c0bd8abadeb38
MatthewGerber <gerber.matthew@gmail.com>
2016-03-18 17:13:39 -0400

    Extra blank lines.

--- ReleaseSensusCheckArgs.sh
+++ ReleaseSensusCheckArgs.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 if [ $# -ne 7 ]; then
+    echo
     echo "Purpose:  Creates a release of Sensus for Android and iOS, based on the current GitHub branch."
     echo ""
     echo "Usage:  ./ReleaseSensus.sh [version] [android keystore path] [android keystore password] [github prerelease] [encryption key] [xamarin insights key] [google play track]"
@@ -13,5 +14,6 @@ if [ $# -ne 7 ]; then
     echo "\t[google play track]:  Google Play track (alpha, beta, production, or rollout)."
     echo ""
     echo "For example (for a prerelease to beta):  ./ReleaseSensus.sh 0.8.0-prerelease /path/to/sensus.keystore keystore_password true 234-23-4-23f-sdf-4 23423423-42342-34-24 beta"
+    echo
     exit 1
 fi
