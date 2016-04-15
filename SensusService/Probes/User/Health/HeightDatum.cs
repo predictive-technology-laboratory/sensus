be35bc214001fdc886e7d37048606d6e7263d0e8
MatthewGerber <gerber.matthew@gmail.com>
2016-04-15 07:09:49 -0400

    Added a missing trigger property name.

--- SensusService/Probes/User/Health/HeightDatum.cs
+++ SensusService/Probes/User/Health/HeightDatum.cs
@@ -23,7 +23,7 @@ namespace SensusService.Probes.User.Health
     {
         private double _heightInches;
 
-        [DoubleProbeTriggerProperty]
+        [DoubleProbeTriggerProperty("Height (Inches)")]
         [Anonymizable(null, new Type[] { typeof(DoubleRoundingTensAnonymizer) }, -1)]
         public double HeightInches
         {
