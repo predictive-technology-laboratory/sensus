242e702dfd0f963ba4b4d75c9726f5797be638ce
MatthewGerber <gerber.matthew@gmail.com>
2016-04-29 10:27:44 -0400

    Moved release script to own directory.

--- Scripts/Release/ReleaseSensusiOS.sh
+++ Scripts/Release/ReleaseSensusiOS.sh
@@ -3,21 +3,21 @@
 . ./ReleaseSensusPreparation.sh
 
 # update Sensus version in plist file
-awk "/<key>CFBundleVersion<\/key>/ {f=1; print; next} f {\$1=\"\t<string>$1</string>\"; f=0} 1" ./Sensus.iOS/Info.plist > tmp && mv tmp ./Sensus.iOS/Info.plist
-awk "/<key>CFBundleShortVersionString<\/key>/ {f=1; print; next} f {\$1=\"\t<string>$1</string>\"; f=0} 1" ./Sensus.iOS/Info.plist > tmp && mv tmp ./Sensus.iOS/Info.plist
+awk "/<key>CFBundleVersion<\/key>/ {f=1; print; next} f {\$1=\"\t<string>$1</string>\"; f=0} 1" ../../Sensus.iOS/Info.plist > tmp && mv tmp ../../Sensus.iOS/Info.plist
+awk "/<key>CFBundleShortVersionString<\/key>/ {f=1; print; next} f {\$1=\"\t<string>$1</string>\"; f=0} 1" ../../Sensus.iOS/Info.plist > tmp && mv tmp ../../Sensus.iOS/Info.plist
 
 # show updates that will be used to build the release
 echo "The following differences will be used to build the iOS release."
 git difftool
 
 # build IPA
-xbuild /p:Configuration=Release /p:Platform=iPhone /p:BuildIpa=true /target:Rebuild ./Sensus.iOS/Sensus.iOS.csproj
+xbuild /p:Configuration=Release /p:Platform=iPhone /p:BuildIpa=true /target:Rebuild ../../Sensus.iOS/Sensus.iOS.csproj
 if [ $? -ne 0 ]; then
     echo "Error building iOS release."
     exit $?;
 fi
 
-ipaPath="./Sensus.iOS/bin/iPhone/Release/SensusiOS-$1.ipa"
+ipaPath="../../Sensus.iOS/bin/iPhone/Release/SensusiOS-$1.ipa"
 
 # get itunes connect username
 read -p "Enter iTunes Connect username:  " -r itunesUsername
@@ -40,8 +40,8 @@ fi
 
 # create/upload iOS dSYM file if upload was successful
 echo "Zipping and uploading dSYM file to Xamarin Insights."
-zip -r ./Sensus.iOS/bin/iPhone/Release/SensusiOS.dSYM.zip ./Sensus.iOS/bin/iPhone/Release/SensusiOS.app.dSYM
-curl -F "dsym=@./Sensus.iOS/bin/iPhone/Release/SensusiOS.dSYM.zip;type=application/zip" "https://xaapi.xamarin.com/api/dsym?apikey=$6"
+zip -r ../../Sensus.iOS/bin/iPhone/Release/SensusiOS.dSYM.zip ../../Sensus.iOS/bin/iPhone/Release/SensusiOS.app.dSYM
+curl -F "dsym=@../../Sensus.iOS/bin/iPhone/Release/SensusiOS.dSYM.zip;type=application/zip" "https://xaapi.xamarin.com/api/dsym?apikey=$6"
 if [ $? -ne 0 ]; then
     echo "Error uploading dSYM file to Xamarin Insights."
     exit $?;
