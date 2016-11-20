ef85f5f3f263a4cb2adf600c8678e4ed6b07f1c6
Mark Rucker <rucker.mark@gmail.com>
2016-11-19 21:58:30 -0500

    New unit tests

---
+++ Sensus.Shared.Tests/Sensus.Shared/UI/Inputs/InputGroupTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using Sensus.UI.Inputs;
+
+namespace Sensus.Tests.UI.Inputs
+{
+    [TestFixture]
+    public class InputGroupTests
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.DoesNotThrow(() => new InputGroup());
+        }
+
+        [Test]
+        public void Test2()
+        {
+            
+        }
+    }
+}
