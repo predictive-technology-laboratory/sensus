fe0f67959f7173e4bd07e0c42dbcabe950d6035b
MatthewGerber <gerber.matthew@gmail.com>
2017-02-12 12:49:30 -0500

    Added privacy policy. Closes #333.

--- Sensus.Shared/UI/HomePage.cs
+++ Sensus.Shared/UI/HomePage.cs
@@ -52,6 +52,29 @@ namespace Sensus.UI
                 await Navigation.PushAsync(new PendingScriptsPage());
             };
 
+            Label privacyPolicyLabel = new Label
+            {
+                Text = "Privacy Policy",
+                HorizontalOptions = LayoutOptions.Center
+            };
+
+            TapGestureRecognizer privacyPolicyTappedRecognizer = new TapGestureRecognizer();
+            privacyPolicyTappedRecognizer.Tapped += async (s, e) =>
+            {
+                ContentPage privacyPolicyPage = new ContentPage
+                {
+                    Title = "Privacy Policy",
+                    Content = new ScrollView
+                    {
+                        Content = new Label { Text = "Immediately following installation, Sensus will not collect, store, or upload any personal information from the device on which it is running. Sensus will upload reports when the app crashes. These reports contain information about the state of the app when it crashed, and Sensus developers will use these crash reports to improve Sensus. These reports do not contain any personal information. After you load a study into Sensus, Sensus will begin collecting data as defined by the study. You will be notified when the study is loaded and is about to start, and you will be asked to confirm that you wish to start the study. This confirmation will summarize the types of data to be collected. You may quit a study and/or uninstall Sensus at any time. Be aware that Sensus is publicly available and that anyone can use Sensus to design a study, which they can then share with others. Studies have the ability to collect personal information, and you should exercise caution when loading any study that you receive." }
+                    }
+                };
+
+                await Navigation.PushAsync(privacyPolicyPage);
+            };
+
+            privacyPolicyLabel.GestureRecognizers.Add(privacyPolicyTappedRecognizer);
+
             Content = new StackLayout
             {
                 Orientation = StackOrientation.Vertical,
@@ -59,7 +82,8 @@ namespace Sensus.UI
                 Children =
                 {
                     studiesButton,
-                    surveysButton
+                    surveysButton,
+                    privacyPolicyLabel
                 }
             };
         }
