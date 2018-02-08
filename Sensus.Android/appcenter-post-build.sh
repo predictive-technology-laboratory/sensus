3704e79c7cdd9104ef070c85ed3e3acca3e3f926
MatthewGerber <gerber.matthew@gmail.com>
2018-02-08 01:05:13 -0500

    Started post-build (GitHub) script.

---
+++ Sensus.Android/appcenter-post-build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# create github release for builds from master, as we're going to release them to the store.
+if [ "$APPCENTER_BRANCH" == "master" ] || [ "$APPCENTER_BRANCH" == "master-ci" ] ; then
+
+
+  
+fi
