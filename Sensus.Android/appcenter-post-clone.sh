a067177ca9b43d1e76425c43ca69640f60c5953d
MatthewGerber <gerber.matthew@gmail.com>
2018-02-07 23:26:31 -0500

    Branch typo.

--- Sensus.Android/appcenter-post-clone.sh
+++ Sensus.Android/appcenter-post-clone.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # set keys for builds from master, as we're going to release them to the store.
-if [ "$APPCENTER_BRANCH" == "master" ] || [ "$APPCENTER_BRANCH" == "master_ci" ] ; then
+if [ "$APPCENTER_BRANCH" == "master" ] || [ "$APPCENTER_BRANCH" == "master-ci" ] ; then
 
   sed -i '' "s/public const string ENCRYPTION_KEY = \"\"/public const string ENCRYPTION_KEY = \"$ENCRYPTION_KEY\"/g" $APPCENTER_SOURCE_DIRECTORY/Sensus.Shared/SensusServiceHelper.cs
   sed -i '' "s/public const string APP_CENTER_KEY_ANDROID = \"\"/public const string APP_CENTER_KEY_ANDROID = \"$APP_CENTER_KEY_ANDROID\"/g" $APPCENTER_SOURCE_DIRECTORY/Sensus.Shared/SensusServiceHelper.cs
