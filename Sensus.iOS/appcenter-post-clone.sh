4585ef666c995e285ed747b422b90f5fb7690aed
MatthewGerber <gerber.matthew@gmail.com>
2018-02-07 23:38:00 -0500

    Revised iOS build script.

--- Sensus.iOS/appcenter-post-clone.sh
+++ Sensus.iOS/appcenter-post-clone.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-# set keys
-sed -i '' "s/public const string ENCRYPTION_KEY = \"\"/public const string ENCRYPTION_KEY = \"$ENCRYPTION_KEY\"/g" $APPCENTER_SOURCE_DIRECTORY/Sensus.Shared/SensusServiceHelper.cs
-sed -i '' "s/public const string APP_CENTER_KEY_IOS = \"\"/public const string APP_CENTER_KEY_IOS = \"$APP_CENTER_KEY_IOS\"/g" $APPCENTER_SOURCE_DIRECTORY/Sensus.Shared/SensusServiceHelper.cs
+# set keys for builds from master, as we're going to release them to the store.
+if [ "$APPCENTER_BRANCH" == "master" ] || [ "$APPCENTER_BRANCH" == "master-ci" ] ; then
+
+  sed -i '' "s/public const string ENCRYPTION_KEY = \"\"/public const string ENCRYPTION_KEY = \"$ENCRYPTION_KEY\"/g" $APPCENTER_SOURCE_DIRECTORY/Sensus.Shared/SensusServiceHelper.cs
+  sed -i '' "s/public const string APP_CENTER_KEY_IOS = \"\"/public const string APP_CENTER_KEY_IOS = \"$APP_CENTER_KEY_IOS\"/g" $APPCENTER_SOURCE_DIRECTORY/Sensus.Shared/SensusServiceHelper.cs
+
+fi
