a28e4e018ce53abd97bdb3025891009a2c7919cd
MatthewGerber <gerber.matthew@gmail.com>
2018-05-29 16:06:37 -0400

    Token var name.

--- Sensus.Android.Tests/appcenter-post-build.sh
+++ Sensus.Android.Tests/appcenter-post-build.sh
@@ -4,7 +4,7 @@
 msbuild $APPCENTER_SOURCE_DIRECTORY/Sensus.Android.Tests.AppCenter/Sensus.Android.Tests.AppCenter.csproj
 
 # log in to the app center
-appcenter login --token $TEST_CLOUD_API_KEY
+appcenter login --token $TEST_CLOUD_API_TOKEN
 
 # submit test
 appcenter test run uitest --app "uva-predictive-technology-lab/sensus-android" --devices "uva-predictive-technology-lab/single-android-device" --app-path $APPCENTER_OUTPUT_DIRECTORY/*.apk --test-series "master" --locale "en_US" --build-dir "$APPCENTER_SOURCE_DIRECTORY/Sensus.Android.Tests.AppCenter/bin/Release" --async
