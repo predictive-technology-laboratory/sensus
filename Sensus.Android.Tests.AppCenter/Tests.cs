d3bb549015cca16861a7ccb600b9db8abc662355
MatthewGerber <gerber.matthew@gmail.com>
2018-05-29 00:18:07 -0400

    Finally have unit testing results back to the UI tester.

--- Sensus.Android.Tests.AppCenter/Tests.cs
+++ Sensus.Android.Tests.AppCenter/Tests.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.IO;
 using System.Linq;
 using NUnit.Framework;
 using Xamarin.UITest;
 using Xamarin.UITest.Android;
-using Xamarin.UITest.Queries;
 
 namespace Sensus.Android.Tests.AppCenter
 {
@@ -16,21 +14,14 @@ namespace Sensus.Android.Tests.AppCenter
         [SetUp]
         public void BeforeEachTest()
         {
-            // TODO: If the Android app being tested is included in the solution then open
-            // the Unit Tests window, right click Test Apps, select Add App Project
-            // and select the app projects that should be tested.
-            app = ConfigureApp
-                .Android
-                // TODO: Update this path to point to your Android app and uncomment the
-                // code if the app is not included in the solution.
-                //.ApkFile ("../../../Android/bin/Debug/UITestsAndroid.apk")
-                .StartApp();
+            app = ConfigureApp.Android.StartApp();
         }
 
         [Test]
-        public void AppLaunches()
+        public void UnitTests()
         {
-            app.Screenshot("First screen.");
+            string log = app.WaitForElement(c => c.All().Marked("sensus-test-log"), timeout: TimeSpan.FromMinutes(2)).FirstOrDefault()?.Text;
+            Console.Out.WriteLine(log);
         }
     }
 }
--- Sensus.Android.Tests/MainActivity.cs
+++ Sensus.Android.Tests/MainActivity.cs
@@ -1,27 +1,92 @@
-﻿using System.Reflection;
-
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Text;
 using Android.App;
+using Android.Content.PM;
 using Android.OS;
+using Android.Widget;
 using Sensus.Android.Tests.SetUp;
 using Xamarin.Android.NUnitLite;
 
 namespace Sensus.Android.Tests
 {
-    [Activity(Label = "Sensus.Android.Tests", MainLauncher = true)]
+    [Activity(Label = "Sensus.Android.Tests", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : TestSuiteActivity
     {
+        private class LogBuilder : TextWriter
+        {
+            public override Encoding Encoding => Encoding.Unicode;
+
+            public StringBuilder Log { get; } = new StringBuilder();
+
+            public override void Write(string value)
+            {
+                base.Write(value);
+
+                lock (Log)
+                {
+                    Log.Append(value);
+                }
+            }
+
+            public override void WriteLine()
+            {
+                base.WriteLine();
+
+                lock(Log)
+                {
+                    Log.AppendLine();
+                }
+            }
+
+            public override void WriteLine(string value)
+            {
+                base.WriteLine(value);
+
+                lock (Log)
+                {
+                    Log.AppendLine(value);
+                }
+            }
+        }
+
+        private LogBuilder _logBuilder = new LogBuilder();
+
         protected override void OnCreate(Bundle bundle)
         {
             SetUpFixture.SetUp();
 
             AddTest(Assembly.GetExecutingAssembly());
 
+            // to add tests in additional assemblies, uncomment/adapt the line below
             // AddTest (typeof (Your.Library.TestClass).Assembly);
 
+            // there is currently an issue where the test runner blocks the UI thread.
             Intent.PutExtra("automated", true);
+
+            // redirect the output to our writer
+            Console.SetOut(_logBuilder);
 
             // Once you called base.OnCreate(), you cannot add more assemblies.
             base.OnCreate(bundle);
+        }
+
+        public override void Finish()
+        {
+            RunOnUiThread(() =>
+            {
+                TextView logView = new TextView(this)
+                {
+                    ContentDescription = "sensus-test-log",
+                    Text = _logBuilder.Log.ToString().Trim()
+                };
+
+                ScrollView logScroll = new ScrollView(this);
+                logScroll.AddView(logView);
+
+                SetContentView(logScroll);
+            });
         }
     }
 }
