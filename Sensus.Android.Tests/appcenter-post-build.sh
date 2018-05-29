7010cc67ed0a701f8afb13f53ba43d8accdd9883
MatthewGerber <gerber.matthew@gmail.com>
2018-05-29 16:52:33 -0400

    Target different devices.

--- Sensus.Android.Tests/appcenter-post-build.sh
+++ Sensus.Android.Tests/appcenter-post-build.sh
@@ -7,4 +7,22 @@ msbuild /p:Configuration=Release $APPCENTER_SOURCE_DIRECTORY/Sensus.Android.Test
 appcenter login --token $TEST_CLOUD_API_TOKEN
 
 # submit test
-appcenter test run uitest --app "uva-predictive-technology-lab/sensus-android" --devices "uva-predictive-technology-lab/single-android-device" --app-path $APPCENTER_OUTPUT_DIRECTORY/*.apk --test-series "master" --locale "en_US" --build-dir "$APPCENTER_SOURCE_DIRECTORY/Sensus.Android.Tests.AppCenter/bin/Release" --async
+
+devices=""
+
+if [ "$APPCENTER_BRANCH" == "develop-ui-test-single-device" ] ; then
+    
+    devices="uva-predictive-technology-lab/single-android-device"
+
+elif [ "$APPCENTER_BRANCH" == "develop-ui-test-lmco-devices" ] ; then
+
+    devices="uva-predictive-technology-lab/lmco-android-test-devices"
+
+else
+
+    echo "Unrecognized branch:  $APPCENTER_BRANCH"
+    exit 1
+
+fi
+
+appcenter test run uitest --app "uva-predictive-technology-lab/sensus-android" --devices "$devices" --app-path $APPCENTER_OUTPUT_DIRECTORY/*.apk --test-series "master" --locale "en_US" --build-dir "$APPCENTER_SOURCE_DIRECTORY/Sensus.Android.Tests.AppCenter/bin/Release" --async
