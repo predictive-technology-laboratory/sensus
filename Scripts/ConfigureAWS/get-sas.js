026b6e67868080e72687e2075f4a7ced174bf0e3
MatthewGerber <gerber.matthew@gmail.com>
2018-09-28 11:31:04 -0400

    Update documentation.

--- Scripts/ConfigureAWS/get-sas.js
+++ Scripts/ConfigureAWS/get-sas.js
@@ -1,9 +1,15 @@
 var util = require('util');
 var crypto = require('crypto');
 
-var url = 'XXXXURLXXXX';                                       // for example:  https://sensus-notifications.servicebus.windows.net/sensus-notifications/messages
-var sharedAccessKeyName = 'DefaultFullSharedAccessSignature';  // the key with full shared access to the notification hub
-var sharedAccessKey = 'XXXXKEYXXXX';                           // the value of the DefaultFullSharedAccessSignature key
+var url = 'XXXXURLXXXX';                                       // https://NAMESPACE.servicebus.windows.net/HUB/messages -- where NAMESPACE is 
+                                                               // your Azure push notification namespace name and HUB is your Azure push 
+                                                               // notification HUB name.
+
+var sharedAccessKeyName = 'DefaultFullSharedAccessSignature';  // the key with full shared access to the notification hub. this should be the 
+                                                               // default value. if you use another key name, replace this value.
+
+var sharedAccessKey = 'XXXXKEYXXXX';                           // the value of the DefaultFullSharedAccessSignature key (e.g., cVRantasldfkjaslkj3flkjelfrz+a3lkjflkj=)
+
 var expiry = new Date(); 
 expiry.setMinutes(expiry.getMinutes() + 5);                    // the signature will be valid for 5 minutes
 var expiryEpoch = expiry instanceof Date ? expiry.getTime() / 1000 : expiry;
