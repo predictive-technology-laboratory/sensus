d86f401c568218ba4a80ab22ecb95395918d05a5
MatthewGerber <gerber.matthew@gmail.com>
2018-10-19 08:34:55 -0400

    Catch exceptions. Fixes #642.

--- Sensus.Android.Shared/Probes/Communication/AndroidSmsOutgoingObserver.cs
+++ Sensus.Android.Shared/Probes/Communication/AndroidSmsOutgoingObserver.cs
@@ -40,78 +40,88 @@ namespace Sensus.Android.Probes.Communication
 
         public override void OnChange(bool selfChange, global::Android.Net.Uri uri)
         {
-            // might be getting null URIs
-            if (uri == null)
+            // this method will be called on the UI thread and thus will crash the app if an exception is
+            // thrown.
+            try
             {
-                return;
-            }
-
-            // for some reason, we get multiple calls to OnChange for the same outgoing text. ignore repeats.
-            if (_mostRecentlyObservedSmsURI != null && uri.ToString() == _mostRecentlyObservedSmsURI)
-            {
-                return;
-            }
-
-            // TODO:  Fix issue #75 -- need to handle MMS. they are structured differently than SMS, and the code below does not work.
-            if (uri.ToString().StartsWith("content://sms/raw"))
-            {
-                return;
-            }
-
-            ICursor cursor = global::Android.App.Application.Context.ContentResolver.Query(uri, null, null, null, null);
-
-            if (cursor.MoveToNext())
-            {
-                // we've been seeing some issues with missing fields. catch any exceptions that occur here and report them.
-                try
+                // might be getting null URIs
+                if (uri == null)
                 {
-                    string protocol = cursor.GetString(cursor.GetColumnIndexOrThrow("protocol"));
-                    int type = cursor.GetInt(cursor.GetColumnIndexOrThrow("type"));
-
-                    int sentMessageType;
-
-                    // see the Backwards Compatibility article for more information
-#if __ANDROID_19__
-                    if (Build.VERSION.SdkInt >= BuildVersionCodes.Kitkat)
-                    {
-                        sentMessageType = (int)SmsMessageType.Sent;  // API level 19
-                    }
-                    else
-#endif
-                    {
-                        sentMessageType = 2;
-                    }
-
-                    if (protocol != null || type != sentMessageType)
-                    {
-                        return;
-                    }
-
-                    string toNumber = cursor.GetString(cursor.GetColumnIndexOrThrow("address"));
-                    long unixTimeMS = cursor.GetLong(cursor.GetColumnIndexOrThrow("date"));
-                    DateTimeOffset dotNetDateTime = new DateTimeOffset(1970, 1, 1, 0, 0, 0, new TimeSpan()).AddMilliseconds(unixTimeMS);
-                    string message = cursor.GetString(cursor.GetColumnIndexOrThrow("body"));
-
-                    _outgoingSmsCallback?.Invoke(new SmsDatum(dotNetDateTime, null, toNumber, message, true));
-
-                    _mostRecentlyObservedSmsURI = uri.ToString();
+                    return;
                 }
-                catch (Exception ex)
+
+                // for some reason, we get multiple calls to OnChange for the same outgoing text. ignore repeats.
+                if (_mostRecentlyObservedSmsURI != null && uri.ToString() == _mostRecentlyObservedSmsURI)
                 {
-                    // something is wrong with our implementation
-                    SensusException.Report(ex);
+                    return;
                 }
-                finally
+
+                // TODO:  Fix issue #75 -- need to handle MMS. they are structured differently than SMS, and the code below does not work.
+                if (uri.ToString().StartsWith("content://sms/raw"))
                 {
-                    // always close cursor
+                    return;
+                }
+
+                ICursor cursor = global::Android.App.Application.Context.ContentResolver.Query(uri, null, null, null, null);
+
+                if (cursor.MoveToNext())
+                {
+                    // we've been seeing some issues with missing fields. catch any exceptions that occur here and report them.
                     try
                     {
-                        cursor.Close();
+                        string protocol = cursor.GetString(cursor.GetColumnIndexOrThrow("protocol"));
+                        int type = cursor.GetInt(cursor.GetColumnIndexOrThrow("type"));
+
+                        int sentMessageType;
+
+                        // see the Backwards Compatibility article for more information
+#if __ANDROID_19__
+                        if (Build.VERSION.SdkInt >= BuildVersionCodes.Kitkat)
+                        {
+                            sentMessageType = (int)SmsMessageType.Sent;  // API level 19
+                        }
+                        else
+#endif
+                        {
+                            sentMessageType = 2;
+                        }
+
+                        if (protocol != null || type != sentMessageType)
+                        {
+                            return;
+                        }
+
+                        string toNumber = cursor.GetString(cursor.GetColumnIndexOrThrow("address"));
+                        long unixTimeMS = cursor.GetLong(cursor.GetColumnIndexOrThrow("date"));
+                        DateTimeOffset dotNetDateTime = new DateTimeOffset(1970, 1, 1, 0, 0, 0, new TimeSpan()).AddMilliseconds(unixTimeMS);
+                        string message = cursor.GetString(cursor.GetColumnIndexOrThrow("body"));
+
+                        _outgoingSmsCallback?.Invoke(new SmsDatum(dotNetDateTime, null, toNumber, message, true));
+
+                        _mostRecentlyObservedSmsURI = uri.ToString();
                     }
-                    catch
+                    catch (Exception ex)
                     {
+                        // something is wrong with our implementation
+                        SensusException.Report(ex);
+                    }
+                    finally
+                    {
+                        // always close cursor
+                        try
+                        {
+                            cursor.Close();
+                        }
+                        catch
+                        {
+                        }
                     }
                 }
+            }
+            catch(Exception)
+            {
+                // something is wrong with our implementation
+                SensusException.Report(ex);
             }
         }
     }
