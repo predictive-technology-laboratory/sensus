6fc2eeda919e4970d8f685b282e9d33a0aca2e80
Ryan Dagostino <rd3fa@cacs.virginia.edu>
2018-10-26 08:05:59 -0400

    #644 working on adding gyroscope probe

---
+++ Sensus.Android.Shared/Probes/Movement/AndroidGyroscopeProbe.cs
@@ -0,0 +1,107 @@
+﻿// Copyright 2014 The Rector & Visitors of the University of Virginia
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Android.Hardware;
+using Sensus.Probes.Movement;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Sensus.Android.Probes.Movement
+{
+    public class AndroidGyroscopeProbe : GyroscopeProbe
+    {
+        private AndroidSensorListener _gyroScopeListener;
+        private static float NS2S = 1.0f / 1000000000.0f;
+        private float[] deltaRotationVector = new float[4];
+        private float timeStamp;
+
+        public AndroidGyroscopeProbe()
+        {
+            _gyroScopeListener = new AndroidSensorListener(SensorType.Gyroscope, null, async e =>
+            {
+                // should get x, y, and z values
+                if (e.Values.Count != 3 || Stabilizing || timeStamp == 0)
+                {
+                    return;
+                }
+
+                IList<float> sensorEventVals = e.Values;
+                //method here to calculate
+                float [] vals = CalculateGyroscope(e, sensorEventVals);
+
+                await StoreDatumAsync(new GyroscopeDatum(DateTimeOffset.UtcNow, vals[0], vals[1],vals[2]));
+            });
+        }
+
+        private float[] CalculateGyroscope(SensorEvent e, IList<float> values)
+        {
+            
+            // This time step's delta rotation to be multiplied by the current rotation
+            // after computing it from the gyro sample data.
+            float dT = (e.Timestamp - timeStamp) * NS2S;
+
+            float x = values[0];
+            float y = values[1];
+            float z = values[2];
+
+            double axisCalc = (x * x) + (y * y) + (z * z);
+
+            float omegaMagnitude = (float)Math.Sqrt(axisCalc);
+
+            if (omegaMagnitude > float.Epsilon)
+            {
+                x /= omegaMagnitude;
+                y /= omegaMagnitude;
+                z /= omegaMagnitude;
+            }
+
+            float thetaOverTwo = omegaMagnitude * dT / 2.0f;
+            float sinThetaOverTwo = (float)Math.Sin(thetaOverTwo);
+            float cosThetaOverTwo = (float)Math.Cos(thetaOverTwo);
+            deltaRotationVector[0] = sinThetaOverTwo * x;
+            deltaRotationVector[1] = sinThetaOverTwo * y;
+            deltaRotationVector[2] = sinThetaOverTwo * z;
+            deltaRotationVector[3] = cosThetaOverTwo;
+
+
+            float[] deltaRotationMatrix = new float[9];
+
+            SensorManager.GetRotationMatrixFromVector(deltaRotationMatrix, deltaRotationVector);
+                        
+
+            return deltaRotationMatrix;
+        }
+
+        protected override async Task InitializeAsync()
+        {
+            await base.InitializeAsync();
+
+            _gyroScopeListener.Initialize(MinDataStoreDelay);
+        }
+
+        protected override async Task StartListeningAsync()
+        {
+            await base.StartListeningAsync();
+
+            _gyroScopeListener.Start();
+        }
+
+        protected override Task StopListeningAsync()
+        {
+            _gyroScopeListener.Stop();
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Sensus.Shared/Probes/Movement/GyroscopeProbe.cs
@@ -0,0 +1,119 @@
+﻿// Copyright 2014 The Rector & Visitors of the University of Virginia
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Syncfusion.SfChart.XForms;
+
+namespace Sensus.Probes.Movement
+{
+    /// <summary>
+    /// Provides acceleration in x, y, and z directions as <see cref="GyroscopeDatum"/> readings.
+    /// </summary>
+    public abstract class GyroscopeProbe : ListeningProbe
+    {
+        private bool _stabilizing;
+
+        protected bool Stabilizing
+        {
+            get { return _stabilizing; }
+        }
+
+        [JsonIgnore]
+        protected override bool DefaultKeepDeviceAwake
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        [JsonIgnore]
+        protected override string DeviceAwakeWarning
+        {
+            get
+            {
+                return "This setting does not affect iOS. Android devices will use additional power to report all updates.";
+            }
+        }
+
+        [JsonIgnore]
+        protected override string DeviceAsleepWarning
+        {
+            get
+            {
+                return "This setting does not affect iOS. Android devices will sleep and pause updates.";
+            }
+        }
+
+        public sealed override string DisplayName
+        {
+            get { return "Gyroscope"; }
+        }
+
+        public sealed override Type DatumType
+        {
+            get { return typeof(AccelerometerDatum); }
+        }
+
+        protected override async Task InitializeAsync()
+        {
+            await base.InitializeAsync();
+
+            _stabilizing = true;
+        }
+
+        protected override async Task StartListeningAsync()
+        {
+            // allow the accelerometer to stabilize...the first few readings can be extremely erratic
+            await Task.Delay(2000);
+            _stabilizing = false;
+
+            // not sure if null is the problem:  https://insights.xamarin.com/app/Sensus-Production/issues/907
+            if (SensusServiceHelper.Get() != null)
+            {
+                SensusServiceHelper.Get().Logger.Log("Gyroscope has finished stabilization period.", LoggingLevel.Normal, GetType());
+            }
+        }
+
+        public override async Task ResetAsync()
+        {
+            await base.ResetAsync();
+
+            _stabilizing = false;
+        }
+
+        protected override ChartSeries GetChartSeries()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override ChartDataPoint GetChartDataPointFromDatum(Datum datum)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override ChartAxis GetChartPrimaryAxis()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override RangeAxisBase GetChartSecondaryAxis()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
