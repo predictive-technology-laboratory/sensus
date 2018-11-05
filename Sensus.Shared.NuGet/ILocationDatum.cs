34a52174f98a0f2e8e53a02043b7e83a4b0902a0
MatthewGerber <gerber.matthew@gmail.com>
2018-11-04 20:50:49 -0500

    Added package.

---
+++ Sensus.Shared.NuGet/ILocationDatum.cs
@@ -0,0 +1,8 @@
+﻿namespace Sensus.Probes.Location
+{
+    public interface ILocationDatum : IDatum
+    {
+        double Latitude { get; set; }
+        double Longitude { get; set; }
+    }
+}
---
+++ Sensus.Shared.NuGet/IScriptProbeAgent.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+namespace Sensus.Probes.User.Scripts
+{
+    public interface IScriptProbeAgent
+    {
+        string Name { get; }
+        string Id { get; }
+
+        void Observe(IDatum datum);
+
+        Task<bool> ShouldDeliverSurvey(IScript script);
+    }
+}
