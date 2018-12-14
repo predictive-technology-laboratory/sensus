9aed6ffce3a8bcb84a7de592a3dedf877a4b6e02
Charles McKinley <clm8v@cacs.virginia.edu>
2018-12-14 16:35:21 -0500

    initial work on #686.  I tested this with a harness but not through a real notification

---
+++ Sensus.Shared/Extensions/JsonConvertExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sensus.Extensions
+{
+    public static class JsonConvertExtensions
+    {
+        public static bool IsValidJson(this string strInput)
+        {
+            var rVal = false;
+            strInput = strInput.Trim();
+            if ((strInput.StartsWith("{") && strInput.EndsWith("}")) || //For object
+                (strInput.StartsWith("[") && strInput.EndsWith("]"))) //For array
+            {
+                try
+                {
+                    var obj = JToken.Parse(strInput);
+                    rVal = true;
+                }
+                catch (Exception ex) { }
+            }
+            return rVal;
+        }
+    }
+}
