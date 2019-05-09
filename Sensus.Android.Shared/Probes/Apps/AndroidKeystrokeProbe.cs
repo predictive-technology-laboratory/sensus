b2bed3003b58411e58aa0695642f8beeda913354
Mehdi Boukhechba <m_boukhechba@esi.dz>
2019-05-09 16:06:34 -0400

    Check if Accessibility service is enabled

--- Sensus.Android.Shared/Probes/Apps/AndroidKeystrokeProbe.cs
+++ Sensus.Android.Shared/Probes/Apps/AndroidKeystrokeProbe.cs
@@ -9,13 +9,30 @@ using Android.AccessibilityServices;
 using Android.Views.Accessibility;
 using Android.Content;
 using Android.App;
+using Android.Views.Accessibility;
+using System.Diagnostics;
+using Android.Text;
+using System.Runtime.Remoting.Contexts;
 
 namespace Sensus.Android.Probes.Apps
 {
     public class AndroidKeystrokeProbe : KeystrokeProbe
     {
+        private AndroidKeystrokeService _accessibilityListener;
+        private DateTime? _accessibilityEventTime;
+
         public AndroidKeystrokeProbe()
         {
+            _accessibilityListener = new AndroidKeystrokeService();
+
+            _accessibilityListener.AccessibilityBroadcast += async (o, key) =>
+            {
+                _accessibilityEventTime = DateTime.Now;
+                Console.WriteLine("***** OnAccessibilityEvent ***** eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee ");
+
+                await SensusServiceHelper.Get().FlashNotificationAsync(key);
+                //await StoreDatumAsync(new TelephonyDatum(DateTimeOffset.UtcNow, TelephonyState.IncomingCall, incomingNumber, null));
+            };
         }
 
         protected override ChartDataPoint GetChartDataPointFromDatum(Datum datum)
@@ -40,16 +57,50 @@ namespace Sensus.Android.Probes.Apps
 
         protected override Task StartListeningAsync()
         {
-            //Intent serviceIntent = new Intent(Application.Context, typeof(AndroidKeystrokeService));
-            //Application.Context.StartService(serviceIntent);
+            //SensusServiceHelper.Get().FlashNotificationAsync("arrive");
+
+            Boolean b = isAccessibilityServiceEnabled();
+            Intent serviceIntent = new Intent(Application.Context, typeof(AndroidKeystrokeService));
+            Application.Context.StartService(serviceIntent);
             return Task.CompletedTask;
         }
 
         protected override Task StopListeningAsync()
         {
-            //Intent serviceIntent = new Intent(Application.Context, typeof(AndroidKeystrokeService));
-            //Application.Context.StopService(serviceIntent);
+            Intent serviceIntent = new Intent(Application.Context, typeof(AndroidKeystrokeService));
+            Application.Context.StopService(serviceIntent);
             return Task.CompletedTask;
+        }
+
+        public Boolean  isAccessibilityServiceEnabled()
+        {
+
+            AccessibilityManager accessibilityManager = (AccessibilityManager)Application.Context.GetSystemService(AccessibilityService.AccessibilityService);
+            IList<AccessibilityServiceInfo> enabledServices = accessibilityManager.GetEnabledAccessibilityServiceList(FeedbackFlags.AllMask);
+
+            //SensusServiceHelper.Get().FlashNotificationAsync(enabledServices.Count.ToString());
+
+            for (int i = 0; i < enabledServices.Count; i++) {
+                AccessibilityServiceInfo e = enabledServices[i];
+                Console.WriteLine("***** OnAccessibilityEvent ***** " + e.ResolveInfo.ServiceInfo.PackageName + "  " + Application.Context.ApplicationInfo.PackageName);
+                if (e.ResolveInfo.ServiceInfo.PackageName == Application.Context.ApplicationInfo.PackageName) { 
+                SensusServiceHelper.Get().FlashNotificationAsync(e.ResolveInfo.ServiceInfo.PackageName.ToString());
+                return true;
+                }
+            }
+
+            //foreach (AccessibilityServiceInfo enabled in enabledServices)
+            //{
+            //    //SensusServiceHelper.Get().FlashNotificationAsync(enabled.ResolveInfo.ServiceInfo.PackageName + "  " + Application.Context.ApplicationInfo.PackageName);
+            //    //Console.WriteLine("***** OnAccessibilityEvent ***** " + enabled.ResolveInfo.ServiceInfo.PackageName + "  " + Application.Context.ApplicationInfo.PackageName);
+
+            //    if (enabled.ResolveInfo.ServiceInfo.PackageName == Application.Context.ApplicationInfo.PackageName)
+            //        SensusServiceHelper.Get().FlashNotificationAsync(enabled.ResolveInfo.ServiceInfo.PackageName.ToString());
+            //    return true;
+            //}
+            
+
+            return false;
         }
     }
 }
--- Sensus.Android.Shared/Probes/Apps/AndroidKeystrokeService.cs
+++ Sensus.Android.Shared/Probes/Apps/AndroidKeystrokeService.cs
@@ -6,6 +6,9 @@ using Android.AccessibilityServices;
 using Sensus.Android;
 using Android.App;
 using Android;
+using Android.Util;
+using Sensus.Probes.Apps;
+using Android.Content;
 
 namespace Sensus.Android.Probes.Apps
 {
@@ -14,10 +17,21 @@ namespace Sensus.Android.Probes.Apps
     public class AndroidKeystrokeService : AccessibilityService
     {
         private AccessibilityServiceInfo info = new AccessibilityServiceInfo();
+        public event EventHandler<string> AccessibilityBroadcast;
         public override void OnAccessibilityEvent(AccessibilityEvent e)
         {
 
-            Console.WriteLine("***** OnAccessibilityEvent ***** "+ e.EventType.ToString());
+            string key = e.Text.ToString();
+            Console.WriteLine("***** OnAccessibilityEvent ***** " + e.EventType.ToString() + "   " + e.PackageName + "  " + e.Text);
+
+            AccessibilityBroadcast?.Invoke(this, key);
+            //StoreDatumAsync(new KeystrokeDatum(DateTimeOffset.UtcNow, heading));
+        }
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+            
         }
 
         public override void OnInterrupt()
@@ -25,11 +39,29 @@ namespace Sensus.Android.Probes.Apps
             throw new NotImplementedException();
         }
 
+        public override ComponentName StartService(Intent service)
+        {
+            SensusServiceHelper.Get().FlashNotificationAsync("start");
+
+            //info.EventTypes = EventTypes.ViewTextChanged;
+            //info.FeedbackType = FeedbackFlags.AllMask;
+            //info.NotificationTimeout = 100;
+            //this.SetServiceInfo(info);
+
+            return base.StartService(service);
+        }
+
+        public override bool StopService(Intent name)
+        {
+            SensusServiceHelper.Get().FlashNotificationAsync("stop");
+            return base.StopService(name);
+        }
+
         protected override void OnServiceConnected()
         {
             base.OnServiceConnected();
             info.EventTypes = EventTypes.ViewTextChanged;
-            info.FeedbackType =FeedbackFlags.AllMask;
+            info.FeedbackType = FeedbackFlags.AllMask;
             info.NotificationTimeout = 100;
             this.SetServiceInfo(info);
 
