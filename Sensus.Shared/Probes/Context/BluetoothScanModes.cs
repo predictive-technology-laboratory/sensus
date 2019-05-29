04eb338f0e35cc6de0db2b4937f5d56061e8a3c5
Benjamin Davidson <bzd3y@virginia.edu>
2019-05-29 08:31:21 -0400

    bluetooth probe #781 #786

---
+++ Sensus.Shared/Probes/Context/BluetoothScanModes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sensus.Probes.Context
+{
+	[Flags]
+	public enum BluetoothScanModes
+	{
+		Classic = 1,
+		LE = 2,
+		All = Classic | LE
+	}
+}
