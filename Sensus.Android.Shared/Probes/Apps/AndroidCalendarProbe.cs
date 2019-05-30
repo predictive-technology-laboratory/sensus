0f044d568b2c5a4c043802734894818b02b8a589
Ryan Dagostino <rd3fa@cacs.virginia.edu>
2019-05-30 12:22:12 -0400

    #783 working on calendar probe

---
+++ Sensus.Android.Shared/Probes/Apps/AndroidCalendarProbe.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using Sensus.Probes.Apps;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.Permissions.Abstractions;
+using Android.Provider;
+using Android.Content;
+using Android.Database;
+using Android.App;
+
+namespace Sensus.Android.Probes.Apps
+{
+    public class AndroidCalendarProbe : CalendarProbe
+    {
+        [JsonIgnore]
+        public override int DefaultPollingSleepDurationMS => (int)TimeSpan.FromMinutes(1).TotalMilliseconds;
+
+        protected override async Task InitializeAsync()
+        {
+            await base.InitializeAsync();
+        }
+
+        protected async override Task<List<Datum>> GetCalendarEvents()
+        {
+
+            // BLE requires location permissions
+            if (await SensusServiceHelper.Get().ObtainPermissionAsync(Permission.Calendar) != PermissionStatus.Granted)
+            {
+                // throw standard exception instead of NotSupportedException, since the user might decide to enable location in the future
+                // and we'd like the probe to be restarted at that time.
+                string error = "Calendar permission is not permitted on this device. Cannot start Calendar probe.";
+                await SensusServiceHelper.Get().FlashNotificationAsync(error);
+                throw new Exception(error);
+            }
+
+            global::Android.Net.Uri eventsUri = CalendarContract.Events.ContentUri;
+
+            string[] eventsProjection = {
+                CalendarContract.Events.InterfaceConsts.Id,
+                CalendarContract.Events.InterfaceConsts.Title,
+                CalendarContract.Events.InterfaceConsts.Dtstart,
+                //CalendarContract.Events.InterfaceConsts.
+            };
+
+            return null;
+        }
+    }
+}
---
+++ Sensus.Shared/Probes/Apps/CalendarProbe.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Syncfusion.SfChart.XForms;
+
+namespace Sensus.Probes.Apps
+{
+    public abstract class CalendarProbe : PollingProbe
+    {
+        public override int DefaultPollingSleepDurationMS => throw new NotImplementedException();
+
+        protected abstract Task<List<Datum>> GetCalendarEvents();
+
+        public override string DisplayName => throw new NotImplementedException();
+
+        public override Type DatumType => throw new NotImplementedException();
+
+        protected override ChartDataPoint GetChartDataPointFromDatum(Datum datum)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override ChartAxis GetChartPrimaryAxis()
+        {
+            throw new NotImplementedException();
+        }
+       
+        protected override RangeAxisBase GetChartSecondaryAxis()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override ChartSeries GetChartSeries()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Task<List<Datum>> PollAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
