612a974fcea2250018192d1f66257ef0c20dc740
Benjamin Davidson <bzd3y@virginia.edu>
2019-08-15 15:29:40 -0400

    #818 application usage probe

---
+++ Sensus.Android.Shared/Probes/Apps/ApplicationUsageDatum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sensus.Android.Probes.Apps
+{
+	public class ApplicationUsageDatum : Datum
+	{
+		public override string DisplayDetail => throw new NotImplementedException();
+
+		public override object StringPlaceholderValue => throw new NotImplementedException();
+
+		public ApplicationUsageDatum(string packageName, string applicationName, string eventType, DateTimeOffset timestamp) : base(timestamp)
+		{
+			PackageName = packageName;
+			ApplicationName = applicationName;
+			EventType = eventType;
+		}
+
+		public string PackageName { get; set; }
+		public string ApplicationName { get; set; }
+		public string EventType { get; set; }
+	}
+}
