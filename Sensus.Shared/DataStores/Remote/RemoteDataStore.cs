539359a7c8796cb5cebe9192c03438e897e170c0
mob3f <mob3f@virginia.edu>
2019-10-14 19:39:52 -0400

    #887

--- Sensus.Shared/DataStores/Remote/RemoteDataStore.cs
+++ Sensus.Shared/DataStores/Remote/RemoteDataStore.cs
@@ -49,7 +49,9 @@ namespace Sensus.DataStores.Remote
 		private float _requiredBatteryChargeLevelPercent;
         private bool _alertUserWhenBackgrounded;
 		private string _userNotificationMessage;
-		private EventHandler<bool> _powerConnectionChanged;
+
+
+        private EventHandler<bool> _powerConnectionChanged;
 		private ConnectivityChangedEventHandler _wifiConnectionChanged;
 		private CancellationTokenSource _powerConnectWriteCancellationToken;
 		private CancellationTokenSource _wifiConnectWriteCancellationToken;
@@ -186,12 +188,12 @@ namespace Sensus.DataStores.Remote
             set { _userNotificationMessage = value; }
         }
 
-        /// <summary>
-        /// Tolerance in milliseconds for running the <see cref="RemoteDataStore"/> before the scheduled 
-        /// time, if doing so will increase the number of batched actions and thereby decrease battery consumption.
-        /// </summary>
-        /// <value>The delay tolerance before.</value>
-        [EntryIntegerUiProperty("Delay Tolerance Before (MS):", true, 60, true)]
+            /// <summary>
+            /// Tolerance in milliseconds for running the <see cref="RemoteDataStore"/> before the scheduled 
+            /// time, if doing so will increase the number of batched actions and thereby decrease battery consumption.
+            /// </summary>
+            /// <value>The delay tolerance before.</value>
+            [EntryIntegerUiProperty("Delay Tolerance Before (MS):", true, 60, true)]
         public int DelayToleranceBeforeMS { get; set; }
 
         /// <summary>
