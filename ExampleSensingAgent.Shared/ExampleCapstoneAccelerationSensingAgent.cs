423602b1321f5b5343f7f44cc8e64d7cbf04581e
Mark Rucker <rucker.mark@gmail.com>
2020-03-17 11:12:19 -0400

    adding fully functional logistic regression in capstone sensing agent

--- ExampleSensingAgent.Shared/ExampleCapstoneAccelerationSensingAgent.cs
+++ ExampleSensingAgent.Shared/ExampleCapstoneAccelerationSensingAgent.cs
@@ -198,6 +198,11 @@ namespace ExampleSensingAgent
         /// Features for the listening window three cycles ago
         /// </summary>
         private Features FeaturesLag3 { get; set; }
+
+        /// <summary>
+        /// The prediction threshold for determining if sensors should be turned on
+        /// </summary>
+        private double Threshold { get; set; }
         #endregion
 
         #region Constructors
@@ -205,6 +210,7 @@ namespace ExampleSensingAgent
         {
             ListeningWindow  = TimeSpan.FromSeconds(5);
             PredictingWindow = TimeSpan.FromSeconds(5);
+            Threshold        = 0.5;
         }
         #endregion
 
@@ -213,6 +219,7 @@ namespace ExampleSensingAgent
         {
             ListeningWindow  = TimeSpan.FromSeconds(double.Parse(policy["cps-listening"].ToString()));
             PredictingWindow = TimeSpan.FromSeconds(double.Parse(policy["cps-predicting"].ToString()));
+            Threshold        = double.Parse(policy["cps-threshold"].ToString());
 
             return Task.CompletedTask;
         }
@@ -266,7 +273,91 @@ namespace ExampleSensingAgent
         /// <returns>true if we should take control of the sensors otherwise false</returns>
         private bool ShouldControlPredictionWindow(Features[] features)
         {
-            return features.Where(f => f != null).Count() < 2;
+            var logOdds     = LogisticCoefficientArray().Zip(LogisticFeatureArray(features), (coeff, feat) => coeff * feat).Sum();
+            var probability = 1 / (1 + Math.Exp(-logOdds));
+
+            return probability > Threshold;
+        }
+
+        /// <summary>
+        /// I have manually entered the coeffecients calculated by Lee and published here
+        /// https://docs.google.com/spreadsheets/d/1PMLEeIJmll0q17AfN-wJtE-kZQkEEPivzmbz6ZqvqbY/edit?usp=sharing
+        /// </summary>
+        /// <returns>the ordered enumeration of coefficients to be multiplied by features in logistic regression</returns>
+        private IEnumerable<double> LogisticCoefficientArray()
+        {
+            yield return 5.63266503524798; //Mean
+            yield return 8.04525608288699; //Median
+            yield return -4.81048518038808; //StdDev
+            yield return 0.8747959277907; //Range
+            yield return -0.239184335512369; //Weekend
+            yield return 0.38258810681683; //Mean_Lag1
+            yield return 0.140387153053747; //Mean_Lag2
+            yield return 0.122013362597331; //Mean_Lag3
+            yield return 0.143413522190335; //Hour_0
+            yield return -0.326810807022531; //Hour_1
+            yield return -0.66023166841557; //Hour_2
+            yield return -1.1781152937892; //Hour_3
+            yield return -1.75599255231115; //Hour_4
+            yield return -1.71331423402301; //Hour_5
+            yield return -1.6073578712332; //Hour_6
+            yield return -0.964254297086296; //Hour_7
+            yield return -0.333204903480208; //Hour_8
+            yield return 0.0897604682713143; //Hour_9
+            yield return 0.275851343057055; //Hour_10
+            yield return 0.39231229693617; //Hour_11
+            yield return 0.534206303176744; //Hour_12
+            yield return 0.550567177237923; //Hour_13
+            yield return 0.445326590625548; //Hour_14
+            yield return 0.481962539399613; //Hour_15
+            yield return 0.562196539344065; //Hour_16
+            yield return 0.499077852599594; //Hour_17
+            yield return 0.628422242015625; //Hour_18
+            yield return 0.461599460854535; //Hour_19
+            yield return 0.480105654312706; //Hour_20
+            yield return 0.547435185098384; //Hour_21
+            yield return 0.464194755942784; //Hour_22
+            yield return 0.390767671628528; //Hour_23
+            yield return -0.242759499623908; //DayOfWeek_0
+            yield return -0.325778210991607; //DayOfWeek_1
+            yield return -0.292001735750749; //DayOfWeek_2
+            yield return -0.319007943271427; //DayOfWeek_3
+            yield return -0.173350299555425; //DayOfWeek_4
+            yield return -0.147612811958076; //DayOfWeek_5
+            yield return -0.0915715235346701; //DayOfWeek_6
+            yield return -1.59208202468202; //intercept
+        }
+
+        /// <summary>
+        /// Converts our array of features into the appropriate form for the logistic regression
+        /// </summary>
+        /// <param name="features">an array of features where the index is the lag</param>
+        /// <returns>the ordered enumeration of features to be multiplied by coefficients in logistic regression</returns>
+        private IEnumerable<double> LogisticFeatureArray(Features[] features)
+        {
+            yield return features[0].Acceleration.Mean;
+            yield return features[0].Acceleration.Median;
+            yield return features[0].Acceleration.StandardDeviation;
+            yield return features[0].Acceleration.Range;
+            yield return Convert.ToDouble(features[0].IsWeekend);
+            
+            for(var lag = 1; lag < 4; lag++)
+            {
+                yield return features[lag]?.Acceleration.Mean ?? 0;
+            }
+            
+            for(var hour = 0; hour < 24; hour++)
+            {
+                yield return Convert.ToDouble(features[0].Hour == hour);
+            }
+
+            for(var day = 0; day < 7; day++)
+            {
+                yield return Convert.ToDouble(features[0].Day == day);
+            }
+
+            // the intercept
+            yield return 1;
         }
         #endregion
     }
