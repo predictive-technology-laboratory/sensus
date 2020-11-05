84ad5b1fff9a82ee27f6b3599e379dad26b2a80a
Benjamin Davidson <bzd3y@virginia.edu>
2020-11-05 16:24:36 -0500

    readonlytextinput

---
+++ Sensus.Shared/UI/Inputs/ReadOnlyTextInput.cs
@@ -0,0 +1,44 @@
+﻿using Sensus.UI.UiProperties;
+using Xamarin.Forms;
+
+namespace Sensus.UI.Inputs
+{
+	public class ReadOnlyTextInput : Input
+	{
+		public ReadOnlyTextInput()
+		{
+			StoreCompletionRecords = false;
+			Complete = true;
+			Required = false;
+		}
+
+		public override object Value
+		{
+			get
+			{
+				return Text;
+			}
+		}
+
+		public override bool Enabled { get; set; }
+
+		public override string DefaultName => "Text";
+
+		[EditorUiProperty("Text", true, 5, true)]
+		public string Text { get; set; }
+
+		public override View GetView(int index)
+		{
+			if (base.GetView(index) == null)
+			{
+				Label label = CreateLabel(-1);
+
+				label.Text = Text;
+
+				base.SetView(label);
+			}
+
+			return base.GetView(index);
+		}
+	}
+}
