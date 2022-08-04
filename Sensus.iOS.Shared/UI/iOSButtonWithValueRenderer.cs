4638a4bd3e41c068be5e911923e5f866a4361a1b
Davidson, Benjamin Zachary (bzd3y) <bzd3y@virginia.edu>
2022-08-04 17:20:34 -0400

    iosbuttonwithvaluerenderer - button resize fix

--- Sensus.iOS.Shared/UI/iOSButtonWithValueRenderer.cs
+++ Sensus.iOS.Shared/UI/iOSButtonWithValueRenderer.cs
@@ -1,5 +1,8 @@
-﻿using Sensus.iOS.UI;
+﻿using CoreGraphics;
+using Foundation;
+using Sensus.iOS.UI;
 using Sensus.UI.Inputs;
+using System;
 using UIKit;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
@@ -19,8 +22,25 @@ namespace Sensus.iOS.UI
 		{
 			base.OnElementChanged(e);
 
+			bool resized = false;
+
 			if (Control != null)
 			{
+				Element.SizeChanged += (s, e) =>
+				{
+					if (resized == false)
+					{
+						CGRect unconstrainedBounds = ((NSString)Control.TitleLabel.Text).GetBoundingRect(new CGSize(double.MaxValue, double.MaxValue), NSStringDrawingOptions.UsesLineFragmentOrigin, new UIStringAttributes { Font = Control.TitleLabel.Font }, null);
+						CGRect constrainedBounds = ((NSString)Control.TitleLabel.Text).GetBoundingRect(new CGSize(Element.Width - Element.Padding.HorizontalThickness, double.MaxValue), NSStringDrawingOptions.UsesLineFragmentOrigin, new UIStringAttributes { Font = Control.TitleLabel.Font }, null);
+
+						double difference = Element.Height - unconstrainedBounds.Height;
+
+						Element.HeightRequest = Math.Max(Element.Height, constrainedBounds.Height + difference);
+
+						resized = true;
+					}
+				};
+
 				Control.TitleLabel.LineBreakMode = UILineBreakMode.WordWrap;
 				Control.TitleLabel.Lines = 0;
 				Control.TitleLabel.TextAlignment = UITextAlignment.Center;
