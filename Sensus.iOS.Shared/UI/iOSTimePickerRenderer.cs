a7b1303e47a43fe2d6963e62601c3d5e6ae7b1c6
Davidson, Benjamin Zachary (bzd3y) <bzd3y@virginia.edu>
2022-08-10 16:45:57 -0400

    iostimepickerrenderer

---
+++ Sensus.iOS.Shared/UI/iOSTimePickerRenderer.cs
@@ -0,0 +1,30 @@
+﻿using Sensus.iOS.UI;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(TimePicker), typeof(iOSTimePickerRenderer))]
+
+namespace Sensus.iOS.UI
+{
+	public class iOSTimePickerRenderer : TimePickerRenderer
+	{
+		public iOSTimePickerRenderer()
+		{
+
+		}
+
+		protected override void OnElementChanged(ElementChangedEventArgs<TimePicker> e)
+		{
+			base.OnElementChanged(e);
+
+			if (Control != null)
+			{
+				if (Control.InputView is UIDatePicker picker)
+				{
+					picker.PreferredDatePickerStyle = UIDatePickerStyle.Wheels;
+				}
+			}
+		}
+	}
+}
