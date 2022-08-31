62715b604b1d19d5b3ec79e2967d90f072d6bdfe
Davidson, Benjamin Zachary (bzd3y) <bzd3y@virginia.edu>
2022-08-30 20:41:06 -0400

    pedometer probe

--- Sensus.Android.Shared/Probes/Movement/AndroidPedometerProbe.cs
+++ Sensus.Android.Shared/Probes/Movement/AndroidPedometerProbe.cs
@@ -7,36 +7,45 @@ using Sensus.Probes.Movement;
 
 namespace Sensus.Android.Probes.Movement
 {
-    class AndroidPedometerProbe : PedometerProbe
-    {
-        private AndroidSensorListener _pedometerListener;
-        public AndroidPedometerProbe()
-        {
+	class AndroidPedometerProbe : PedometerProbe
+	{
+		private readonly AndroidSensorListener _pedometerListener;
 
-            _pedometerListener = new AndroidSensorListener(SensorType.StepCounter, async e =>
-            {
+		private float _lastValue;
 
-                await StoreDatumAsync(new PedometerDatum(DateTimeOffset.UtcNow, e.Values[0] ));
-            });
-        }
+		public AndroidPedometerProbe()
+		{
+			_lastValue = 0;
 
-        protected override async Task InitializeAsync()
-        {
-            await base.InitializeAsync();
-            _pedometerListener.Initialize(MinDataStoreDelay);
-        }
-        protected override async Task StartListeningAsync()
-        {
-            await base.StartListeningAsync();
+			_pedometerListener = new AndroidSensorListener(SensorType.StepCounter, async e =>
+			{
+				if (_lastValue > 0)
+				{
+					await StoreDatumAsync(new PedometerDatum(DateTimeOffset.UtcNow, e.Values[0] - _lastValue));
+				}
 
-            _pedometerListener.Start();
-        }
+				_lastValue = e.Values[0];
+			});
+		}
 
-        protected override async Task StopListeningAsync()
-        {
-            await base.StopListeningAsync();
-            _pedometerListener.Stop();
+		protected override async Task InitializeAsync()
+		{
+			await base.InitializeAsync();
 
-        }
-    }
+			_pedometerListener.Initialize(MinDataStoreDelay);
+		}
+		protected override async Task StartListeningAsync()
+		{
+			await base.StartListeningAsync();
+
+			_pedometerListener.Start();
+		}
+
+		protected override async Task StopListeningAsync()
+		{
+			await base.StopListeningAsync();
+
+			_pedometerListener.Stop();
+		}
+	}
 }
