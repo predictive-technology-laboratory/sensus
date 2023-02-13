b58db07e276209b8e2ae511c3cb27ad0fc931d5a
Davidson, Benjamin Zachary (bzd3y) <bzd3y@virginia.edu>
2023-02-13 15:40:16 -0500

    removed Xamarin.Firebase.Iid - fixed warnings

--- Sensus.Android.Shared/Notifications/FirebaseNotificationService.cs
+++ Sensus.Android.Shared/Notifications/FirebaseNotificationService.cs
@@ -23,68 +23,82 @@ using Sensus.Notifications;
 
 namespace Sensus.Android.Notifications
 {
-    [Service(Exported = false)]
-    [IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
-    public class FirebaseNotificationService : FirebaseMessagingService
-    {
-        public async override void OnMessageReceived(RemoteMessage message)
-        {
-            AndroidSensusServiceHelper serviceHelper = null;
+	[Service(Exported = false)]
+	[IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
+	public class FirebaseNotificationService : FirebaseMessagingService
+	{
+		public override async void OnNewToken(string token)
+		{
+			// update push notification registrations using the new token. as this 
+			// is a service, we're not exactly sure when it will be started. so 
+			// the service helper might not be immediately available.
 
-            try
-            {
-                // based on log messages, it looks like the os might destroy the service component of the application
-                // but leave the rest of the application (e.g., the service helper) intact and resident in memory. 
-                // if this happens then the serivce helper will be present, but the service itself will be destroyed. 
-                // this may also mean that the protocols are stopped. regardless, we desire for the service to always
-                // be running, as this ensures that the app will continue as a foreground service. so, ask the os to 
-                // start the service any time a push notification is received. this should be a no-op if the service
-                // is already running. don't ask for the service to be stopped in case no protocols are running, as
-                // it could just be the case that a push notification arrives late after the user has stopped protocols.
-                AndroidSensusService.Start(false);
+			if (SensusServiceHelper.Get() is AndroidSensusServiceHelper serviceHelper)
+			{
+				serviceHelper.SetPushNotificationToken(token);
 
-                serviceHelper = SensusServiceHelper.Get() as AndroidSensusServiceHelper;
+				await SensusServiceHelper.Get()?.UpdatePushNotificationRegistrationsAsync(CancellationToken.None);
+			}
+		}
 
-                // if we just started the service above, then it's likely that the service helper will not yet be 
-                // initialized (it must be deserialized, which is slow). in this case, just bail out and wait for
-                // the next push notification to arrive, at which time the service helper will hopefully be ready.
-                if (serviceHelper == null)
-                {
-                    SensusServiceHelper.Get().Logger.Log("Service helper not initialized following receipt of push notification and service start.", LoggingLevel.Normal, GetType());
-                    return;
-                }
+		public async override void OnMessageReceived(RemoteMessage message)
+		{
+			AndroidSensusServiceHelper serviceHelper = null;
 
-                // acquire wake lock before this method returns to ensure that the device does not sleep prematurely, 
-                // interrupting any execution requested by the push notification. the service 
-                serviceHelper.KeepDeviceAwakeAsync().Wait();
+			try
+			{
+				// based on log messages, it looks like the os might destroy the service component of the application
+				// but leave the rest of the application (e.g., the service helper) intact and resident in memory. 
+				// if this happens then the serivce helper will be present, but the service itself will be destroyed. 
+				// this may also mean that the protocols are stopped. regardless, we desire for the service to always
+				// be running, as this ensures that the app will continue as a foreground service. so, ask the os to 
+				// start the service any time a push notification is received. this should be a no-op if the service
+				// is already running. don't ask for the service to be stopped in case no protocols are running, as
+				// it could just be the case that a push notification arrives late after the user has stopped protocols.
+				AndroidSensusService.Start(false);
 
-                PushNotification pushNotification = new PushNotification
-                {
-                    Id = message.Data["id"],
-                    ProtocolId = message.Data["protocol"],
-                    Update = bool.Parse(message.Data["update"]),
-                    Title = message.Data["title"],
-                    Body = message.Data["body"],
-                    Sound = message.Data["sound"]
-                };
+				serviceHelper = SensusServiceHelper.Get() as AndroidSensusServiceHelper;
 
-                // backend key might be blank
-                string backendKeyString = message.Data["backend-key"];
-                if (!string.IsNullOrWhiteSpace(backendKeyString))
-                {
-                    pushNotification.BackendKey = new Guid(backendKeyString);
-                }
+				// if we just started the service above, then it's likely that the service helper will not yet be 
+				// initialized (it must be deserialized, which is slow). in this case, just bail out and wait for
+				// the next push notification to arrive, at which time the service helper will hopefully be ready.
+				if (serviceHelper == null)
+				{
+					SensusServiceHelper.Get().Logger.Log("Service helper not initialized following receipt of push notification and service start.", LoggingLevel.Normal, GetType());
+					return;
+				}
 
-                await SensusContext.Current.Notifier.ProcessReceivedPushNotificationAsync(pushNotification, CancellationToken.None);
-            }
-            catch (Exception ex)
-            {
-                SensusException.Report("Exception while processing remote notification:  " + ex.Message, ex);
-            }
-            finally
-            {
-                serviceHelper?.LetDeviceSleepAsync().Wait();
-            }
-        }
-    }
+				// acquire wake lock before this method returns to ensure that the device does not sleep prematurely, 
+				// interrupting any execution requested by the push notification. the service 
+				serviceHelper.KeepDeviceAwakeAsync().Wait();
+
+				PushNotification pushNotification = new PushNotification
+				{
+					Id = message.Data["id"],
+					ProtocolId = message.Data["protocol"],
+					Update = bool.Parse(message.Data["update"]),
+					Title = message.Data["title"],
+					Body = message.Data["body"],
+					Sound = message.Data["sound"]
+				};
+
+				// backend key might be blank
+				string backendKeyString = message.Data["backend-key"];
+				if (!string.IsNullOrWhiteSpace(backendKeyString))
+				{
+					pushNotification.BackendKey = new Guid(backendKeyString);
+				}
+
+				await SensusContext.Current.Notifier.ProcessReceivedPushNotificationAsync(pushNotification, CancellationToken.None);
+			}
+			catch (Exception ex)
+			{
+				SensusException.Report("Exception while processing remote notification:  " + ex.Message, ex);
+			}
+			finally
+			{
+				serviceHelper?.LetDeviceSleepAsync().Wait();
+			}
+		}
+	}
 }
