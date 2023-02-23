e8a201248b35a4fc139d8c941e8a366f32aeb65c
Davidson, Benjamin Zachary (bzd3y) <bzd3y@virginia.edu>
2023-02-23 10:21:58 -0500

    iOSImageMetadataProbe - refactor/fix

--- Sensus.iOS.Shared/Probes/Apps/iOSImageMetadataProbe.cs
+++ Sensus.iOS.Shared/Probes/Apps/iOSImageMetadataProbe.cs
@@ -1,25 +1,27 @@
 ﻿using Sensus.Probes.Apps;
 using System.Threading.Tasks;
 using Photos;
-using CoreFoundation;
+using System.IO;
+using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Sensus.iOS.Probes.Apps
 {
+	[SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>")]
 	public class iOSImageMetadataProbe : ImageMetadataProbe
 	{
 		private iOSPhotoLibraryChangeObserver _changeObserver;
-		private DispatchQueue _dispatchQueue;
 
 		public iOSImageMetadataProbe()
 		{
-			
+
 		}
 
 		protected override async Task InitializeAsync()
 		{
 			await base.InitializeAsync();
-			_dispatchQueue = new DispatchQueue("com.sensus.imagemetadataprobe");
-			_changeObserver = new iOSPhotoLibraryChangeObserver(this, _dispatchQueue);
+
+			_changeObserver = new iOSPhotoLibraryChangeObserver(this);
 		}
 
 		protected override async Task StartListeningAsync()
@@ -37,6 +39,57 @@ namespace Sensus.iOS.Probes.Apps
 			await base.StopListeningAsync();
 
 			PHPhotoLibrary.SharedPhotoLibrary.UnregisterChangeObserver(_changeObserver);
+		}
+
+		public async Task CreateAndStoreDatumAsync(string path, int width, int height, string mimeType, DateTime timestamp)
+		{
+			ImageMetadataDatum datum = new(timestamp)
+			{
+				Width = width,
+				Height = height,
+				MimeType = mimeType
+			};
+
+			if (File.Exists(path))
+			{
+				using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+				{
+					if (mimeType == JPEG_MIME_TYPE)
+					{
+						SetExifData(fs, ref datum);
+					}
+
+					if (StoreImages)
+					{
+						datum.ImageBase64 = await GetImageBase64(fs);
+					}
+				}
+			}
+
+			await StoreDatumAsync(datum);
+		}
+		public async Task CreateAndStoreDatumAsync(string path, int width, int height, string mimeType, int duration, DateTime timestamp)
+		{
+			ImageMetadataDatum datum = new(timestamp)
+			{
+				Width = width,
+				Height = height,
+				Duration = duration,
+				MimeType = mimeType
+			};
+
+			if (File.Exists(path))
+			{
+				using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+				{
+					if (StoreVideos)
+					{
+						datum.ImageBase64 = await GetImageBase64(fs);
+					}
+				}
+			}
+
+			await StoreDatumAsync(datum);
 		}
 	}
 }
