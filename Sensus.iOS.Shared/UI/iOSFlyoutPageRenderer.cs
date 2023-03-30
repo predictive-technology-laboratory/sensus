d4d49d82bc4d32618545a25792038d1796a8b34e
Davidson, Benjamin Zachary (bzd3y) <bzd3y@virginia.edu>
2023-03-30 10:40:01 -0400

    iOS - fix for menu appearing behind status bar

---
+++ Sensus.iOS.Shared/UI/iOSFlyoutPageRenderer.cs
@@ -0,0 +1,37 @@
+﻿using Sensus.iOS.UI;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(FlyoutPage), typeof(iOSFlyoutPageRenderer))]
+
+namespace Sensus.iOS.UI
+{
+	public class iOSFlyoutPageRenderer : PhoneFlyoutPageRenderer
+	{
+		private bool _hasAppeared;
+
+		public iOSFlyoutPageRenderer()
+		{
+			_hasAppeared = false;
+		}
+
+		public override void ViewDidAppear(bool animated)
+		{
+			base.ViewDidAppear(animated);
+
+			if (_hasAppeared == false)
+			{
+				IVisualElementRenderer flyoutRenderer = Platform.GetRenderer(((FlyoutPage)Element).Flyout);
+
+				UIView superView = flyoutRenderer.NativeView.Superview;
+
+				flyoutRenderer.NativeView.RemoveFromSuperview();
+
+				superView.AddSubview(flyoutRenderer.NativeView);
+
+				_hasAppeared = false;
+			}
+		}
+	}
+}
