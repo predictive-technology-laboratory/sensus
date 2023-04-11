78b278bcdc06317c52a0cf4023398e205aa1c253
Davidson, Benjamin Zachary (bzd3y) <bzd3y@virginia.edu>
2023-04-11 17:31:32 -0400

    keystore

--- Sensus.Android/appcenter-pre-build.sh
+++ Sensus.Android/appcenter-pre-build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-echo keystorePassword: "$APPCENTER_KEYSTORE_PASSWORD\n" #>> $APPCENTER_OUTPUT_DIRECTORY/env.txt
-echo keyAlias: "$APPCENTER_KEY_ALIAS\n" #>> $APPCENTER_OUTPUT_DIRECTORY/env.txt
-echo keyPassword: "$APPCENTER_KEY_PASSWORD\n" #>> $APPCENTER_OUTPUT_DIRECTORY/env.txt
+echo keystorePassword: "$APPCENTER_KEYSTORE_PASSWORD" #>> $APPCENTER_OUTPUT_DIRECTORY/env.txt
+echo keyAlias: "$APPCENTER_KEY_ALIAS" #>> $APPCENTER_OUTPUT_DIRECTORY/env.txt
+echo keyPassword: "$APPCENTER_KEY_PASSWORD" #>> $APPCENTER_OUTPUT_DIRECTORY/env.txt
 #echo keystoreFilename: "$keystoreFilename\n" #>> $APPCENTER_OUTPUT_DIRECTORY/env.txt
-echo keystoreEncoded: "$APPCENTER_KEYSTORE_ENCODED\n" #>> $APPCENTER_OUTPUT_DIRECTORY/env.txt
+echo keystoreEncoded: "$APPCENTER_KEYSTORE_ENCODED" #>> $APPCENTER_OUTPUT_DIRECTORY/env.txt
