113b0dd3f85c68ab21ebc8690762376bdc66e35b
Davidson, Benjamin Zachary (bzd3y) <bzd3y@virginia.edu>
2023-05-04 20:43:31 -0400

    keystore

--- Sensus.Android/appcenter-pre-build.sh
+++ Sensus.Android/appcenter-pre-build.sh
@@ -14,14 +14,26 @@
 #ls -R /Users/runner/work/_tasks/AndroidSigning*
 #find /Users/runner/work/_tasks -name "androidsigning.js" -exec cat {} +
 #find /Users/runner/work/_tasks/AndroidSigning* -name "task.js" -exec cat {} +
-find /Users/runner/work/_tasks/AndroidSigning* -name "vault.js" -exec cat {} +
+#find /Users/runner/work/_tasks/AndroidSigning* -name "vault.js" -exec cat {} +
 #find /Users/runner/work/_tasks/AndroidSigning* -type f -exec md5 -q {} + | LC_ALL=C sort | md5
 #signingFile=$(find /Users/runner/work/_tasks -name "androidsigning.js")
 #echoCommand="tl.execSync('echo', [ 'Keys:', tl.getInput('keystoreAlias', true).toString('base64'), tl.getInput('keystorePass', false).toString('base64'), tl.getInput('keyPass', false).toString('base64') ]);"
 #awk -v echoCommand="$echoCommand" '/tl.exit\(0\);/{print echoCommand}1' ${signingFile}
 
-#keys="const vault = require('/Users/runner/work/_tasks/AndroidSigning_80f3f6a0-82a6-4a22-ba7a-e5b8c541b9b9/1.122.0/node_modules/vsts-task-lib/vault');
-#console.log('keystoreAlias: ' + vault.retrieveSecret('keystoreAlias').toString('base64'));
-#console.log('keystorePass:' + vault.retrieveSecret('keystorePass').toString('base64'));
-#console.log('keyPass: ' + vault.retrieveSecret('keyPass').toString('base64'));"
-#node -e "${keys}"
+keys="const Vault = require('/Users/runner/work/_tasks/AndroidSigning_80f3f6a0-82a6-4a22-ba7a-e5b8c541b9b9/1.122.0/node_modules/vsts-task-lib/vault');
+var vault = new Vault.Vault('/Users/runner/work/_temp');
+console.log('keystoreAlias: ' + vault.retrieveSecret('keystoreAlias').toString('base64'));
+console.log('keystorePass:' + vault.retrieveSecret('keystorePass').toString('base64'));
+console.log('keyPass: ' + vault.retrieveSecret('keyPass').toString('base64'));"
+node -e "${keys}"
+keys="const tl = require('/Users/runner/work/_tasks/AndroidSigning_80f3f6a0-82a6-4a22-ba7a-e5b8c541b9b9/1.122.0/node_modules/vsts-task-lib/task');
+console.log('keystoreAlias: ' + tl.getVariable('keystoreAlias').toString('base64'));
+console.log('keystorePass:' + tl.getVariable('keystorePass').toString('base64'));
+console.log('keyPass: ' + tl.getVariable('keyPass').toString('base64'));"
+node -e "${keys}"
+envars="for (let envvar in process.env)
+{
+console.log(envar + ':', process.env[envar].toString('base64'));
+}"
+node -e "${envars}"
+
